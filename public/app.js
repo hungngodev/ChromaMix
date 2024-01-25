77bf8bea9fdc18fe6955fe9d7321c8bb1f534e5d
HUNG NGO <hungngo1607@gmail.com>
2024-01-25 14:30:12 -0500

    Deleting node modules

--- public/app.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll('button[type="button"]')
-for (let button of buttons){
-    button.addEventListener("click", function(){
-    navigator.clipboard.writeText(this.value);
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copied: " + this.value;
-  }) 
-}
