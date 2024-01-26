615154309803b3a5513247a8b60f282b6dc4acc6
HUNG NGO <hungngo1607@gmail.com>
2024-01-25 22:25:32 -0500

    fixing navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,14 +19,60 @@ const Header = () => {
   return (
     <>
       <div className="navigation">
+      <div className="waves-container">
+          <div>
+            <svg
+              className="waves"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 24 150 28"
+              preserveAspectRatio="none"
+              shapeRendering="auto"
+            >
+              <defs>
+                <path
+                  id="gentle-wave"
+                  d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                />
+              </defs>
+              <g className="parallax">
+                <use
+                  xlinkHref="#gentle-wave"
+                  x="48"
+                  y="0"
+                  fill="rgba(255,255,255,0.7)"
+                />
+                <use
+                  xlinkHref="#gentle-wave"
+                  x="48"
+                  y="3"
+                  fill="rgba(255,255,255,0.5)"
+                />
+                <use
+                  xlinkHref="#gentle-wave"
+                  x="48"
+                  y="5"
+                  fill="rgba(255,255,255,0.3)"
+                />
+                <use
+                  xlinkHref="#gentle-wave"
+                  x="48"
+                  y="7"
+                  fill="#Fff"
+                  id="whiteWaves"
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
         <nav className="header">
           <NavLink to="/">
             <motion.svg
               variants={svgVariants}
               initial="hidden"
               animate="visible"
-              width="133"
-              height="42"
+              width="400"
+              height="100"
               viewBox="0 0 135 42"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -75,53 +121,6 @@ const Header = () => {
           </ul>
         </nav>
         <div id="progress-bar" />
-        <div id="progress-bar2" />
-        <div className="waves-container">
-          <div>
-            <svg
-              className="waves"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 24 150 28"
-              preserveAspectRatio="none"
-              shapeRendering="auto"
-            >
-              <defs>
-                <path
-                  id="gentle-wave"
-                  d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-                />
-              </defs>
-              <g className="parallax">
-                <use
-                  xlinkHref="#gentle-wave"
-                  x="48"
-                  y="0"
-                  fill="rgba(255,255,255,0.7)"
-                />
-                <use
-                  xlinkHref="#gentle-wave"
-                  x="48"
-                  y="3"
-                  fill="rgba(255,255,255,0.5)"
-                />
-                <use
-                  xlinkHref="#gentle-wave"
-                  x="48"
-                  y="5"
-                  fill="rgba(255,255,255,0.3)"
-                />
-                <use
-                  xlinkHref="#gentle-wave"
-                  x="48"
-                  y="7"
-                  fill="#Fff"
-                  id="whiteWaves"
-                />
-              </g>
-            </svg>
-          </div>
-        </div>
       </div>
     </>
   );
